4283dbde13506ae0d24b707bf3d117d8e519e53a
Aleksey Galatin <aleksey_galatin@epam.com>
2020-07-18 01:22:09 +0300

    added home and policy pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PolicyComponent } from './policy';
+import { HomeComponent } from './home';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'policy', component: PolicyComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
 import { GamesComponent } from './games/games.component';
+import { PolicyComponent } from './policy';
+import { HomeComponent } from './home';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoadingScreenComponent,
     GamesComponent,
+    PolicyComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
