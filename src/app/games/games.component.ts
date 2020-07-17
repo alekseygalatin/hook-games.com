f745abd4109c881170f81b696a648051ae71c3e3
Aleksey Galatin <aleksey_galatin@epam.com>
2020-07-17 22:40:54 +0300

    created game model

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+enum GameType {
+  mobile = "Mobile",
+  pc = "PC"
+}
+
+class Game {
+  constructor(name:string, genre:string, description:string, type:GameType) {
+    this.name = name;
+    this.genre = genre;
+    this.description = description;
+    this.type = type;
+  }
+  name:string;
+  genre:string;
+  description:string;
+  type:GameType;
+
+  isMobile(): boolean {
+    return this.type == GameType.mobile;
+  }
+}
+
 @Component({
   selector: 'games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
+
+  games: Array<Game> = [
+    new Game("More Or Less Space", "Runner", "Description", GameType.mobile)
+  ];
 
   constructor() { }
 
